fd1d77bfa3a7d3878d71f666b6077138a2914ee7
LucasPontees <eulpontes@gmail.com>
2024-09-13 22:56:45 -0400

    tela de cadastro de atividades

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="page-wrapper">
-    <div class="content container-fluid">
-      <main id="Home-page">
-        <button
-          class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded shadow-lg transition-transform duration-200 ease-in-out transform hover:scale-105"
+  <div class="bg-gray-100 p-5 rounded shadow-md max-w-lg h-full max-h-100...">
+    <h2 class="text-2xl font-bold mb-6 text-gray-700 text-center">Cadastro de Atividade</h2>
+
+    <form action="#" method="POST">
+      <!-- Título -->
+      <div class="mb-4">
+        <label for="titulo" class="block text-gray-700 font-medium mb-2">Título</label>
+        <input
+          type="text"
+          id="titulo"
+          name="titulo"
+          class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+          placeholder="Digite o título da atividade"
+          required
+        />
+      </div>
+
+      <!-- Descrição -->
+      <div class="mb-4">
+        <label for="descricao" class="block text-gray-700 font-medium mb-2">Descrição</label>
+        <textarea
+          id="descricao"
+          name="descricao"
+          rows="4"
+          class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+          placeholder="Descreva a atividade"
+          required
+        ></textarea>
+      </div>
+
+      <!-- Status -->
+      <div class="mb-4">
+        <label for="status" class="block text-gray-700 font-medium mb-2">Status</label>
+        <select
+          id="status"
+          name="status"
+          class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
         >
-          Click Me
+          <option value="pendente">Alta</option>
+          <option value="concluida">Baixa</option>
+          <option value="em_progresso">Média</option>
+        </select>
+      </div>
+
+      <!-- Botão Enviar -->
+      <div class="mb-4">
+        <button
+          type="submit"
+          class="w-full bg-indigo-600 text-white font-bold py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
+        >
+          Enviar
         </button>
-        <div class="text-center py-10">
-          <h1 class="text-4xl font-bold text-blue-500">
-            Hello, Tailwind with Vue and Vite!
-          </h1>
-        </div>
-      </main>
-    </div>
+      </div>
+    </form>
   </div>
 </template>
