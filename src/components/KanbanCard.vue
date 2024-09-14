8566b8c587f63fea5779f9b60983636e3c78c2c9
LucasPontees <eulpontes@gmail.com>
2024-09-13 22:10:48 -0400

    adicionado os estilos dos cards

--- src/components/KanbanCard.vue
+++ src/components/KanbanCard.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="bg-white p-4 shadow-md rounded-lg mb-4 hover:bg-gray-100">
-      <h3 class="text-lg font-semibold">{{ task.title }}</h3>
-      <p class="text-gray-600">{{ task.description }}</p>
+    <div class="bg-white p-4 rounded-lg mb-4 group relative">
+      <h3 class="text-lg font-semibold px-1 relative z-10 group-hover:text-white">{{ task.title }}</h3>
+      <span class="absolute left-0 bottom-0 w-full h-0.5 transition-all bg-indigo-600 rounded-lg z-0 group-hover:h-full"></span>
+        <p class="relative z-10 group-hover:text-white">{{ task.description }}</p>
     </div>
   </template>
   
@@ -16,8 +17,3 @@
     },
   };
   </script>
-  
-  <style scoped>
-  /* Adicione estilos específicos do componente aqui, se necessário */
-  </style>
-  
--- src/components/KanbanColumn.vue
+++ src/components/KanbanColumn.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="bg-gray-100 p-4 rounded-lg flex flex-col h-full hover:bg-gray-800">
-      <h2 class="text-xl font-semibold mb-4">{{ title }}</h2>
+    <div class="bg-gray-100 p-4 rounded-lg flex flex-col h-full">
+      <h2 class="text-xl font-semibold mb-4 text-center">{{ title }}</h2>
       <div class="flex-1 overflow-auto">
         <KanbanCard v-for="task in tasks" :key="task.id" :task="task" />
       </div>
@@ -27,8 +27,4 @@
     },
   };
   </script>
-  
-  <style scoped>
-  /* Adicione estilos específicos do componente aqui, se necessário */
-  </style>
   
